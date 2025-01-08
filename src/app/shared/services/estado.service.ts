89b5ca5565f76c51b34b1b88088a490d3f0a0445
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-01-08 15:11:59 -0300

    service estado created

---
+++ src/app/shared/services/estado.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { State } from '../interfaces/state.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class EstadoService {
+  constructor(private http:HttpClient) { }
+
+  async getEstados() {
+    return await lastValueFrom(this.http.get(`${environment.BACKEND_URL}/api0/v0/states`));
+  }
+}
