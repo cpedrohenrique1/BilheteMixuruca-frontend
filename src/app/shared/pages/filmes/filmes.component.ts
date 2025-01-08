2e2f14cd29a834916e6edc3928e07e314371811e
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-01-08 15:11:44 -0300

    component filmes created

---
+++ src/app/shared/pages/filmes/filmes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmes',
+  imports: [],
+  templateUrl: './filmes.component.html',
+  styleUrl: './filmes.component.css'
+})
+export class FilmesComponent {
+
+}
