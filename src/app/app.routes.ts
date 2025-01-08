576f89bd660e9bd3533d5dac03f786f5f3a7c477
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-01-08 15:12:05 -0300

    rotas configuradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/pages/home/home.component';
+import { FilmesComponent } from './shared/pages/filmes/filmes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'filmes',
+        component: FilmesComponent
+    }
+];
