1a17c390f7ec86acd22a8b5caf530195cc9255f5
Pedro Henrique <88724009+cpedrohenrique1@users.noreply.github.com>
2025-01-09 22:22:15 +0000

    update: atualizacao sessionComponente

--- src/app/shared/pages/sessions/sessions.component.ts
+++ src/app/shared/pages/sessions/sessions.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { Data } from '../../interfaces/data.interface';
+import { DiaDaSemana } from '../../interfaces/diadasemana.interface';
 import { ItemComponent } from '../../_components/item/item.component';
+import { SessionService } from '../../services/session.service';
+import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { Theater } from '../../interfaces/theater.interface';
 
 @Component({
   selector: 'app-sessions',
@@ -9,11 +13,32 @@ import { ItemComponent } from '../../_components/item/item.component';
   styleUrl: './sessions.component.css'
 })
 export class SessionsComponent {
-  datas: Data[] = [];
-sessions: any;
+  constructor (private session_service: SessionService, private route:ActivatedRoute){}
+  idCity!: string | null;
+  idMovie!: string | null;
+  dia_da_semana: DiaDaSemana[] = [];
+  cinema: Theater[] = [];
+
+  sessions: any;
   onClickData(_t6: any) {
   throw new Error('Method not implemented.');
   }
   nome_filme: string = 'nome filme';
-
+  async ngOnInit() {
+    this.idCity = this.route.snapshot.paramMap.get('idCity');
+    this.idMovie = this.route.snapshot.paramMap.get('idMovie');
+    try{
+      await firstValueFrom(this.session_service.getAllSessions(this.idCity, this.idMovie)).then(response => {
+        this.dia_da_semana = response;
+      });
+    }catch(error: any) {
+      console.log(error.message);
+    }
+    for (let index = 0; index < this.dia_da_semana.length; index++) {
+      for (let i = 0; i < this.dia_da_semana[index].theaters.length; i++) {
+        this.cinema.push(this.dia_da_semana[index].theaters[i]);
+      }
+    }
+    console.log(this.cinema);
+  }
 }
