dfce3fc2c39e18b88e241fb3c032970fddad7a82
Pedro Henrique <88724009+cpedrohenrique1@users.noreply.github.com>
2025-01-11 18:24:25 +0000

    add: implement getPrice method in SessionService and refactor URL usage

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { DiaDaSemana } from '../interfaces/diadasemana.interface';
+import { Price } from '../interfaces/price.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,24 @@ import { DiaDaSemana } from '../interfaces/diadasemana.interface';
 export class SessionService {
 
   constructor(private http: HttpClient) { }
+  URL: string = environment.BACKEND_URL;
 
   getAllSessions(idCity: string | null, idMovie: string | null) {
     try{
       if (idCity === null || idMovie === null){
         throw new Error();
       }
-      return this.http.get<DiaDaSemana[]>(`${environment.BACKEND_URL}/api0/v0/sessions/city/${idCity}/event/${idMovie}/partnership/home`);
+      return this.http.get<DiaDaSemana[]>(`${this.URL}/api0/v0/sessions/city/${idCity}/event/${idMovie}/partnership/home`);
     }catch(error:any){
       throw new Error('Não foi possível requerer as sessões');
+    }
+  }
+
+  getPrice(idSession: string, idSection: string) {
+    try {
+      return this.http.get<Price>(`${this.URL}/api1/v1/sessions/${idSession}/sections/${idSection}/tickets`);
+    }catch(error: any) {
+      throw new Error("Não foi possível requerer os valores");
     }
   }
 }
