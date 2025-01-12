d89eb01d4c7bfaa68c735db30f2869e6603730f1
Pedro Henrique <88724009+cpedrohenrique1@users.noreply.github.com>
2025-01-12 20:37:11 +0000

    add: nova interface de Sessions

---
+++ src/app/shared/interfaces/sessionsPayload.interface.ts
@@ -0,0 +1,9 @@
+export interface SessionsPayload {
+    horario: string;
+    dia_da_semana: string;
+    data: string;
+    precoInteira: number;
+    precoMeia: number;
+    nome_cinema: string;
+    urlBuy: string;
+}
