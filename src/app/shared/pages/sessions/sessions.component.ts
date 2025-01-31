a0d75d075e44ee8c46200cc7d154316ceb0d3b30
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-01-31 17:16:26 -0300

    refactor: improve session handling logic and enhance response processing in sessions component

--- src/app/shared/pages/sessions/sessions.component.ts
+++ src/app/shared/pages/sessions/sessions.component.ts
@@ -13,77 +13,104 @@ import { SessionsPayload } from '../../interfaces/sessionsPayload.interface';
   styleUrl: './sessions.component.css'
 })
 export class SessionsComponent {
-  constructor (private session_service: SessionService, private route:ActivatedRoute){}
+  constructor(private session_service: SessionService, private route: ActivatedRoute) { }
   idCity!: string | null;
   idMovie!: string | null;
   nome_filme!: string | null;
   dia_da_semana: DiaDaSemana[] = [];
   todas_sessoes: SessionsPayload[] = [];
   show_sessions: SessionsPayload[] = [];
+
   onClickData(item: DiaDaSemana | null) {
     this.show_sessions = [];
-    if (item == null) {
-      for (let i = 0; i < this.todas_sessoes.length; i++) {
-        this.show_sessions.push(this.todas_sessoes[i]);
-      }
-      return;
-    }
     for (let i = 0; i < this.todas_sessoes.length; i++) {
       this.show_sessions.push(this.todas_sessoes[i]);
     }
-    this.show_sessions = this.show_sessions.filter(session => session.dia_da_semana === item.dayOfWeek);
+    if (item == null) {
+      return;
+    }
+    this.show_sessions = this.show_sessions.filter(
+      session => session.dia_da_semana === item.dayOfWeek
+    );
   }
+
+  sortResponse(response: DiaDaSemana[]): DiaDaSemana[] {
+    return response.sort((a, b) => {
+      const getMinPrice = (dia: DiaDaSemana) => {
+        let minPrice = Infinity;
+        dia.theaters.forEach(theater => {
+          theater.rooms.forEach(room => {
+            room.sessions.forEach(session => {
+              if (session.price < minPrice) {
+                minPrice = session.price;
+              }
+            });
+          });
+        });
+        return minPrice;
+      };
+
+      const minPriceA = getMinPrice(a);
+      const minPriceB = getMinPrice(b);
+
+      return minPriceA - minPriceB;
+    });
+  }
+
+  splitResponse(response: DiaDaSemana[]): DiaDaSemana[] {
+    // response will be cut in the half
+    const half = Math.floor(response.length / 2);
+    return response.splice(0, half);
+  }
+
   async ngOnInit() {
     this.nome_filme = this.route.snapshot.paramMap.get('urlMovie');
+    if (this.nome_filme == null) {
+      this.nome_filme = '';
+    } else {
+      let nome_filme_temp: string[] = this.nome_filme.split("");
+      for (let i = 0; i < this.nome_filme.length; i++) {
+        if (this.nome_filme[i] == "-") {
+          nome_filme_temp[i] = " ";
+        }
+        this.nome_filme = nome_filme_temp.join("");
+      }
+    }
     this.idCity = this.route.snapshot.paramMap.get('idCity');
     this.idMovie = this.route.snapshot.paramMap.get('idMovie');
-    // get dias da semana
-    try{
+    try {
       await firstValueFrom(this.session_service.getAllSessions(this.idCity, this.idMovie)).then(response => {
-        this.dia_da_semana = response;
+        this.dia_da_semana = this.splitResponse(this.sortResponse(response));
+        console.log(this.dia_da_semana);
       });
-      for (let i = 0; i < this.dia_da_semana.length; i++) {
-        for (let j = 0; j < this.dia_da_semana[i].theaters.length; j++) {
-          for (let k = 0; k < this.dia_da_semana[i].theaters[j].rooms.length; k++) {
-            for (let l = 0; l < this.dia_da_semana[i].theaters[j].rooms[k].sessions.length; l++) {
-              if (this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].enabled){
-                let session_payload: SessionsPayload = {
-                  data: '',
-                  dia_da_semana: '',
-                  horario: '',
-                  nome_cinema: '',
+      for (const dia of this.dia_da_semana) {
+        for (const theater of dia.theaters) {
+          for (const room of theater.rooms) {
+            for (const session of room.sessions) {
+              if (session.enabled) {
+                const session_payload: SessionsPayload = {
+                  data: session.date.dayAndMonth,
+                  dia_da_semana: session.date.dayOfWeek,
+                  horario: session.date.hour,
+                  nome_cinema: theater.name,
                   precoInteira: 0,
                   precoMeia: 0,
-                  urlBuy: ''
+                  urlBuy: session.siteURL
                 };
-                await firstValueFrom(this.session_service.getPrice(this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].id, this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].defaultSector)).then(response => {
-                  for (let iterator = 0; iterator < response.default.length; iterator++) {
-                    if (response.default[iterator].groupName === "Inteira") {
-                      session_payload.precoInteira = response.default[iterator].price;
-                    }
-                    if (response.default[iterator].groupName === "Meia") {
-                      session_payload.precoMeia = response.default[iterator].price;
-                    }
-                    if (response.default[iterator].id === "2258") {
-                      session_payload.precoInteira = response.default[iterator].price;
-                    }
-                    if (response.default[iterator].id === "2259") {
-                      session_payload.precoMeia = response.default[iterator].price;
-                    }
-                    if (response.default[iterator].id === "3356") {
-                      session_payload.precoInteira = response.default[iterator].price;
-                    }
-                    if (response.default[iterator].id === "3355") {
-                      session_payload.precoMeia = response.default[iterator].price;
-                    }
+
+                const response = await firstValueFrom(this.session_service.getPrice(session.id, session.defaultSector));
+                for (const price of response.default) {
+                  if ("Inteira" === price.groupName || ["1", "2258", "3356"].includes(price.id)) {
+                    session_payload.precoInteira = price.price;
                   }
-                });
-                session_payload.nome_cinema = this.dia_da_semana[i].theaters[j].name;
-                session_payload.horario = this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].date.hour;
-                session_payload.dia_da_semana = this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].date.dayOfWeek;
-                session_payload.data = this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].date.dayAndMonth;
-                session_payload.urlBuy = this.dia_da_semana[i].theaters[j].rooms[k].sessions[l].siteURL;
-                this.todas_sessoes.push(session_payload);
+                  if ("Meia" === price.groupName || ["2", "2259", "3355"].includes(price.id)) {
+                    session_payload.precoMeia = price.price;
+                  }
+                }
+
+                if (session_payload.precoInteira !== 0) {
+                  this.todas_sessoes.push(session_payload);
+                }
               }
             }
           }
@@ -93,8 +120,7 @@ export class SessionsComponent {
       for (let i = 0; i < this.todas_sessoes.length; i++) {
         this.show_sessions.push(this.todas_sessoes[i]);
       }
-      console.log(this.show_sessions);
-    }catch(error: any) {
+    } catch (error: any) {
       console.log(error.message);
     }
   }
