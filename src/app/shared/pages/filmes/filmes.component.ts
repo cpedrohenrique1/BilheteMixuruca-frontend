d9239dccc55ac11433cb834733b0e3dbaa92cd1b
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2026-02-15 16:53:58 -0300

    refactor: improved user experience with loading feedback

--- src/app/shared/pages/filmes/filmes.component.ts
+++ src/app/shared/pages/filmes/filmes.component.ts
@@ -18,14 +18,26 @@ export class FilmesComponent {
   router: ActivatedRoute = inject(ActivatedRoute);
   filmeService: FilmeService = inject(FilmeService);
   route: Router = inject(Router);
+  loading = signal<boolean>(true);
+  loadingComponent = signal<any[]>([]);
+
+  addLoadingComponent(){
+    for (let i = 0; i < 10; i++){
+      setTimeout(() => {
+        this.loadingComponent.update(prev => [...prev, {id: i}]);
+      }, i * 1000);
+    }
+  }
 
   async ngOnInit() {
+    this.addLoadingComponent();
     this.id = this.router.snapshot.paramMap.get('idCity');
     try {
       this.filmeService.getCartaz(this.id).subscribe(response => {
         for (let i = 0; i < response.length; i++) {
           if (response[i].id === "2"){
             this.filme.set(response[i].events);
+            this.loading.set(false);
           }
         }
       });
