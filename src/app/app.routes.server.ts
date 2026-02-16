0f91cf2f4e56ac08adf9b84d39e2e0d4b30a4cac
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2026-02-16 12:30:06 -0300

    refactor: best seo and changed favicon

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -7,10 +7,10 @@ export const serverRoutes: ServerRoute[] = [
   },
   {
     path: 'filmes/:idCity',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Server
   },
   {
     path: 'filmes/:idCity/sessoes/:idMovie/:urlMovie',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Server
   }
 ];
