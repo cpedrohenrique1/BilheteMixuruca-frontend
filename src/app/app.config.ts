66f7ca74874f056c56c2d493a8c3f0bcadf3656b
Pedro Henrique <88724009+cpedrohenrique1@users.noreply.github.com>
2026-03-02 15:11:24 -0300

    Improved SEO & PWA support (#3)
    * chore: installed tailwindcss
    
    * chore: installed daisyUi
    
    * fix: adapted all .css files to use tailwind now
    
    * refactor: using tailwind & daisyUi on home page
    
    * refactor: using new components instead of only one supercomponent, changed route sessions to sessoes
    
    * refactor: better responsivity
    
    * refactor: styled movie-sessions and fixed some responsivity errors
    
    * chore: changed zone.js to zoneless
    
    * refactor: improved user experience with loading feedback
    
    * fix: screen glitch when clicked in date buttons
    
    * fix: button on sessions page now works
    
    * refactor: best seo and changed favicon
    
    * refactor: better accessibility & fixed minor bugs
    
    * refactor: fixed minor bugs & security problems
    
    * refactor: improved UI/UX with dark & light theme
    
    * feat: add pwa support
    
    * fix: fixed logo icons
    
    * add: robots.txt & improved SEO
    
    * add: google ownership with Search Console

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideRouter(routes), provideHttpClient(withFetch()), provideClientHydration(withEventReplay())]
+  providers: [provideZonelessChangeDetection(), provideRouter(routes), provideHttpClient(withFetch()), provideClientHydration(withEventReplay()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
